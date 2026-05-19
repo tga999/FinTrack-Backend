caba9b9e759c591a644d42b5512e2ab8828e17a1
thiago <thiagogalleguillo9@gmail.com>
2026-05-19 19:14:22 -0300

    middleware agregado

--- src/infraestructura/routes/auth.routes.ts
+++ src/infraestructura/routes/auth.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express'
 import { registro, login } from '../controllers/auth.controller'
+import { verificarToken, RequestConUsuario } from '../middlewares/auth.middleware'
+
 
 const router = Router()
 
 //Rutas de autenticación
 router.post('/registro', registro)
 router.post('/login',login)
+
+//Ruta protegida
+
+router.get('/perfil', verificarToken, (req: RequestConUsuario, res) => {
+    res.json({mensaje: 'Ruta protegida', usuarioId: req.usuarioId})
+})
 
 export default router
 
